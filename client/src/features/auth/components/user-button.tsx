ee1cd6320ed0e32cb891a3d7a64fbe147934391a
Shad <Shadifmg@gmail.com>
2025-02-13 17:39:31 -0600

    Feat: Added shadcn avatar and dropdown menu components

---
+++ client/src/features/auth/components/user-button.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+
+const UserButton = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserButton
