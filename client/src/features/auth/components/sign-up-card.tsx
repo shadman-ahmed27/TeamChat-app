42b67767a6e55a15396700a3c67d9e97935ccf71
Shad <Shadman>
2025-02-12 21:21:07 -0600

    Feat: Created Sign up card. Each sign in and sign up card changes state to the other card when clicking on the bottom sign in or sign up button

--- client/src/features/auth/components/sign-up-card.tsx
+++ client/src/features/auth/components/sign-up-card.tsx
@@ -1,7 +1,96 @@
-import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { SignInFlow } from "../types";
 
-const SignUpCard = () => {
-  return <div>Sign Up Card</div>;
+interface SignUpCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+const SignUnCard = ({ setState }: SignUpCardProps) => {
+  return (
+    <Card className="w-full h-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Sign up to continue</CardTitle>
+        <CardDescription>
+          Use email or another service to login.
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form className="space-y-2.5">
+          <Input
+            disabled={false}
+            value={""}
+            onChange={() => {}}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input
+            disabled={false}
+            value={""}
+            onChange={() => {}}
+            placeholder="Password"
+            type="password"
+            required
+          />
+          <Input
+            disabled={false}
+            value={""}
+            onChange={() => {}}
+            placeholder="Confirm Password"
+            type="password"
+            required
+          />
+          <Button type="submit" className="w-full" size={"lg"} disabled={false}>
+            Continue
+          </Button>
+        </form>
+        <Separator />
+        <div>
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant={"outline"}
+            size={"lg"}
+            className="w-full relative"
+          >
+            <FcGoogle className="absolute top-3 left-2.5" />
+            Continue with Google
+          </Button>
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            variant={"outline"}
+            size={"lg"}
+            className="w-full relative"
+          >
+            <FaGithub className="absolute top-3 left-2.5" />
+            Continue with Github
+          </Button>
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Already have an account?{" "}
+          <span
+            onClick={() => setState("signIn")}
+            className="text-sky-700 hover:underline cursor-pointer"
+          >
+            Sign in
+          </span>
+        </p>
+      </CardContent>
+    </Card>
+  );
 };
 
-export default SignUpCard;
+export default SignUnCard;
