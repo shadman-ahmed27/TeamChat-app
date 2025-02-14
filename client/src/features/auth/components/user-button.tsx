318c52ffd3ba0dd27e14457721d9fb4bc7295a15
Shad <Shadifmg@gmail.com>
2025-02-13 18:25:49 -0600

    Feat: Added the user profile imagecircle and logout button when clicking with functionality.

--- client/src/features/auth/components/user-button.tsx
+++ client/src/features/auth/components/user-button.tsx
@@ -1,14 +1,50 @@
-"use client"
+"use client";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useCurrentUser } from "@/features/hooks/use-current-user";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { Loader, LogOut } from "lucide-react";
 
 const UserButton = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { signOut } = useAuthActions();
+  const { data, isLoading } = useCurrentUser();
 
-export default UserButton
+  if (isLoading) {
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  const { image, name, email } = data;
+
+  const avatarFallback = name!.charAt(0).toUpperCase();
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="size-10 hover:opacity-75 transition">
+          <AvatarImage alt={name} src={image} />
+          <AvatarFallback className="bg-sky-500 text-white">
+            {avatarFallback}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center" side="right" className="w-60">
+        <DropdownMenuItem onClick={() => signOut()} className="h-10">
+          <LogOut className="size-4 mr-2" />
+          Log Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserButton;
